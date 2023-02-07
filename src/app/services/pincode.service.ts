de8b7ce082dc56a9ac1892a148d54687e724883f
AnithaSwamy <anithaswamy97@gmail.com>
2023-02-07 15:06:45 +0530

    update

--- src/app/services/pincode.service.ts
+++ src/app/services/pincode.service.ts
@@ -9,17 +9,17 @@ import { POffice } from '../shared/postoffice';
 
 export class PincodeService {
 
-  private baseURL: string = "https://api.postalpincode.in"
+  private baseURL: string = "https://api.postalpincode.in/"
 
   constructor(private http: HttpClient) { }
 
   getPostOfcByCode(pincode: any): any {
-    return this.http.get<POffice>(this.baseURL + '/pincode/' + pincode)
+    return this.http.get<POffice>(this.baseURL + 'pincode/' + pincode)
       .pipe(catchError((error: HttpErrorResponse) => throwError(error)));
   }
 
   getPostOfcByLocation(pincode: any): any {
-    return this.http.get<POffice>(this.baseURL + '/postoffice/' + pincode)
+    return this.http.get<POffice>(this.baseURL + 'postoffice/' + pincode)
       .pipe(catchError((error: HttpErrorResponse) => throwError(error)));
   }
 }
